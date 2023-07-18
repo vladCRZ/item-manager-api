6303a439a5f0316a0d5eca5a71a1ba3d1a6a68cd
Sergio Cruz <vlad.crz@gmail.com>
2023-07-17 19:00:44 -0600

    Adding OpenAPI documentation

--- src/main/java/com/vscg/itemmanagerapiv3/ItemManagerApiV3Application.java
+++ src/main/java/com/vscg/itemmanagerapiv3/ItemManagerApiV3Application.java
@@ -1,9 +1,12 @@
 package com.vscg.itemmanagerapiv3;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Item manager API", version = "2.0", description = "Crud API for management of IT company items"))
 public class ItemManagerApiV3Application {
 
     public static void main(String[] args) {
