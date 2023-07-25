06d26eecc390a1bb3034c2ef933fbca92b8d9bed
Sergio Cruz <vlad.crz@gmail.com>
2023-07-25 02:08:01 -0600

    Adding JWT authentication for securing exposed REST API.

---
+++ src/main/java/com/vscg/itemmanagerapiv3/service/JwtUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.vscg.itemmanagerapiv3.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("vlad".equals(username)) {
+            return new User("vlad", "$2a$10$G6tKpR85fCYwJYP6RxArn.0IHmblNugCYy73pvHsUHfwtanVBGUSC",
+                    new ArrayList<>());
+        }
+        if ("javainuse".equals(username)) {
+            return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
